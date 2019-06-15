335be6ee7e1b7d578219beaa317c5f31603f1fc7
WildanKurniadi <danprogramming10@gmail.com>
2019-06-15 14:52:42 +0700

    #40 Submission5 - add Application Widget

--- app/src/main/java/com/wildanka/moviecatalogue/view/ReminderActivity.java
+++ app/src/main/java/com/wildanka/moviecatalogue/view/ReminderActivity.java
@@ -15,8 +15,8 @@ import com.wildanka.moviecatalogue.util.SharedPref;
 
 public class ReminderActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
     private static final String TAG = "ReminderActivity";
-    public static final String alarmTimeDaily = "14:28";
-    public static final String alarmTimeRelease = "14:28";
+    public static final String alarmTimeDaily = "07:00";
+    public static final String alarmTimeRelease = "08:00";
     private AlarmReceiver alarmReceiver;
     private SharedPref sharedPref;
     @Override
